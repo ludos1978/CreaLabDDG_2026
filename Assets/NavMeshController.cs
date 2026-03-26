d9441918da6ba688ceb4e5ddec9364b3b9569f05
Reto Spoerri <rspoerri@nouser.org>
2026-03-26 16:10:07 +0100

    patrolling agent

--- Assets/NavMeshController.cs
+++ Assets/NavMeshController.cs
@@ -1,21 +1,31 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NavMeshController : MonoBehaviour
 {
 
     public UnityEngine.AI.NavMeshAgent navMeshAgent;
-    public GameObject targetObject;
+    // public GameObject targetObject;
+    public List<GameObject> targetObjects;
+    public int targetIndex = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+      navMeshAgent.SetDestination(targetObjects[targetIndex].transform.position);
+
     }
 
     void GoThere ()
     {
       // targetObject
-      navMeshAgent.SetDestination(targetObject.transform.position);
+
+      if (navMeshAgent.remainingDistance < 0.1f)
+      {
+        Debug.Log("Reached");
+        targetIndex = (targetIndex + 1) % targetObjects.Count;
+        navMeshAgent.SetDestination(targetObjects[targetIndex].transform.position);
+      }
     }
 
     // Update is called once per frame
