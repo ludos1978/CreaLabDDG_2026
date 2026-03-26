e285aa58b78d02ddd5a678f7e7d3ff9ef9ec6608
Reto Spoerri <rspoerri@nouser.org>
2026-03-26 17:10:27 +0100

    .

--- Assets/NavMeshController.cs
+++ Assets/NavMeshController.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class NavMeshController : MonoBehaviour
@@ -8,29 +10,66 @@ public class NavMeshController : MonoBehaviour
     // public GameObject targetObject;
     public List<GameObject> targetObjects;
     public int targetIndex = 0;
+    public GameObject chaseTargetObject;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    public float startChase = 0.0f;
+
+    public enum CHARACTERSTATE {
+      PATROLLING,
+      CHASING,
+      RETURNING
+    }
+    public CHARACTERSTATE characterState;
+
+    void Start() {
       navMeshAgent.SetDestination(targetObjects[targetIndex].transform.position);
-
+      characterState = CHARACTERSTATE.PATROLLING;
     }
 
-    void GoThere ()
-    {
-      // targetObject
+    void Update() {
+      switch (characterState) {
+        case CHARACTERSTATE.PATROLLING:
+          Debug.Log("Patrolling");
+          
+          GameObject foundPlayer = FindPlayer();
+          if (foundPlayer != null) {
+            characterState = CHARACTERSTATE.CHASING;
+            chaseTargetObject = foundPlayer;
+            navMeshAgent.SetDestination(chaseTargetObject.transform.position);
+            startChase = Time.time;
+          }
+          GoThere();
+          break;
 
-      if (navMeshAgent.remainingDistance < 0.1f)
-      {
+        case CHARACTERSTATE.CHASING:
+          Debug.Log("Chasing");
+          if ((Time.time - startChase) > 10f) {
+            characterState = CHARACTERSTATE.PATROLLING;
+          }
+          navMeshAgent.SetDestination(chaseTargetObject.transform.position);
+          break;
+
+        case CHARACTERSTATE.RETURNING:
+          break;
+      }
+    }
+
+    GameObject FindPlayer () {
+      RaycastHit[] hits = Physics.SphereCastAll(this.transform.position, 2f, this.transform.forward, 3f);
+      foreach (RaycastHit hit in hits) {
+        if (hit.collider.gameObject.tag == "Player") {
+          return hit.collider.gameObject;
+        }
+      }
+      return null;
+    }
+
+    void GoThere () {
+      if (navMeshAgent.remainingDistance < 0.1f) {
         Debug.Log("Reached");
         targetIndex = (targetIndex + 1) % targetObjects.Count;
         navMeshAgent.SetDestination(targetObjects[targetIndex].transform.position);
       }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        GoThere();
-    }
 }
